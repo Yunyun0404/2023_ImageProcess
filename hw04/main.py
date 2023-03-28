cc901160d0ea59f2c881137263a6e009664f3d99
yunyun0404 <v04040507@gmail.com>
2023-03-28 22:01:17 +0800

    fix gray to color

--- hw04/main.py
+++ hw04/main.py
@@ -4,18 +4,18 @@ import numpy as np
 k = 0.7
 
 if __name__=="__main__":
-    img = cv2.imread('human.jpg',cv2.IMREAD_GRAYSCALE)
-    averageBlur = cv2.blur(img,(9,9))
-    mediumBlur = cv2.medianBlur(img,9)
+    img = cv2.imread('human.jpg', cv2.IMREAD_COLOR)
+    averageBlur = cv2.blur(img,(15,15))
+    mediumBlur = cv2.medianBlur(img,15)
 
     cv2.imshow("origin",img)
-    cv2.imshow("blur",averageBlur)
+    cv2.imshow("averageBlur",averageBlur)
     cv2.imshow("mediumBlur",mediumBlur)
-    cv2.imwrite('blur.jpg', averageBlur)
+    cv2.imwrite('averageBlur.jpg', averageBlur)
     cv2.imwrite('mediumBlur.jpg', mediumBlur)
 
-    unsharpMaskingAverage = (img - averageBlur)*k + img
-    unsharpMaskingMedian = (img - mediumBlur)*k + img
+    unsharpMaskingAverage = cv2.addWeighted(img,k+1,averageBlur,-k,0)
+    unsharpMaskingMedian = cv2.addWeighted(img,k+1,mediumBlur,-k,0)
     cv2.imshow("unsharpMaskingAverage",unsharpMaskingAverage.astype(np.uint8))
     cv2.imshow("unsharpMaskingMedian",unsharpMaskingMedian.astype(np.uint8))
     cv2.imwrite('unsharpMaskingAverage.jpg', unsharpMaskingAverage.astype(np.uint8))
