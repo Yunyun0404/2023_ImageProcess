ef724e52a6d9908061e3b568619b61d8d58f4dcc
yunyun0404 <v04040507@gmail.com>
2023-04-27 00:15:02 +0800

    edit hw08

--- hw08/main.py
+++ hw08/main.py
@@ -2,26 +2,13 @@ import cv2
 import numpy as np
 
 
-def myThreshold(img, threshold_value, max_value, threshold_type):
-    output = np.zeros_like(img)
-
-    for i in range(img.shape[0]):
-        for j in range(img.shape[1]):
-            if threshold_type == cv2.THRESH_BINARY:
-                if img[i,j] > threshold_value:
-                    output[i,j] = max_value
-                else:
-                    output[i,j] = 0
-            elif threshold_type == cv2.THRESH_BINARY_INV:
-                if img[i,j] > threshold_value:
-                    output[i,j] = 0
-                else:
-                    output[i,j] = max_value
-    return output
+def myThreshold(img):
+    _, thresh = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    return thresh
 
 if __name__ == '__main__':
     img = cv2.imread('flower.jpeg', cv2.IMREAD_GRAYSCALE)
-    threshold= myThreshold(img, 127, 255, cv2.THRESH_BINARY)
+    threshold= myThreshold(img)
 
     cv2.imshow('Flower', img)
     cv2.imshow('Thresholded', threshold)
